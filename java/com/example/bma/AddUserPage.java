901700954ef59af9bfbff4c3c284323575c0cd3e
Gan Kai Lun <gankailun00@gmail.com>
2022-11-20 12:09:58 +0800

    Add User Page & Minor changes

---
+++ java/com/example/bma/AddUserPage.java
@@ -0,0 +1,78 @@
+package com.example.bma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.bma.databinding.ActivityAddUserPageBinding;
+
+public class AddUserPage extends AppCompatActivity {
+
+    private DBHandler db;
+    private ActivityAddUserPageBinding mAddUserPageBinding;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_add_user_page);
+        mAddUserPageBinding = ActivityAddUserPageBinding.inflate(getLayoutInflater());
+        setContentView(mAddUserPageBinding.getRoot());
+
+        //initialize database
+        db = new DBHandler(this);
+
+        //add text watcher to edit text
+        mAddUserPageBinding.nameEditText.addTextChangedListener(textWatcher);
+        mAddUserPageBinding.passwordEditText.addTextChangedListener(textWatcher);
+
+        mAddUserPageBinding.btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //get input from edit text
+                String name = mAddUserPageBinding.nameEditText.getText().toString();
+                String ps= mAddUserPageBinding.passwordEditText.getText().toString();
+
+                //add the user into database
+                boolean addUser = db.addUser(name,ps);
+
+                if(addUser == true){
+                    Toast.makeText(AddUserPage.this,"User has been added!",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(AddUserPage.this,"Please try again!",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+
+
+    public TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            String name = mAddUserPageBinding.nameEditText.getText().toString();
+            String ps= mAddUserPageBinding.passwordEditText.getText().toString();
+            //if all field is not empty , then set the button to enabled
+            if (!name.isEmpty() && !ps.isEmpty()){
+                mAddUserPageBinding.btnSave.setEnabled(true);
+            }
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
+    };
+}
--- java/com/example/bma/SingleFragmentActivity.java
+++ java/com/example/bma/SingleFragmentActivity.java
@@ -3,10 +3,12 @@ package com.example.bma;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,6 +23,7 @@ public abstract class SingleFragmentActivity extends AppCompatActivity {
 
     protected abstract Fragment createFragment();
     private BottomNavigationView mBottomNavigationView;
+    private String login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,10 +46,7 @@ public abstract class SingleFragmentActivity extends AppCompatActivity {
 
         // The value will be default as empty string because for
         // the very first time when the app is opened, there is nothing to show
-        String login = sh.getString("login","");
-
-
-
+        login = sh.getString("login","");
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,productListFragment).commit();
 
@@ -54,23 +54,30 @@ public abstract class SingleFragmentActivity extends AppCompatActivity {
 
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                SharedPreferences sh = getSharedPreferences("MySharedPref", MODE_PRIVATE);
+
+                // The value will be default as empty string because for
+                // the very first time when the app is opened, there is nothing to show
+                login = sh.getString("login","");
+
                 switch (item.getItemId()){
                     case R.id.homepage:
                         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,productListFragment).commit();
                         return true;
 
                     case R.id.menu:
-
-                        if (login == "user"){
+                        String menu_code = login.toString();
+                        if (menu_code.equalsIgnoreCase("user")){
                             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,userMenuFragment).commit();
                             return true;
-
                         }
-                        else if (login == "admin"){
+                        else if (menu_code.equalsIgnoreCase("admin")){
                             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,adminMenuFragment).commit();
                             return true;
+                        }else {
+                            Log.d("MyApp2",login);
+                            return true;
                         }
-
 
                     case R.id.person:
                         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,profileFragment).commit();
@@ -102,79 +109,4 @@ public abstract class SingleFragmentActivity extends AppCompatActivity {
     }
 
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        MenuInflater inflater = getMenuInflater();
-//        inflater.inflate(R.menu.bottom_nav_menu, menu);
-//        return true;
-//    }
-//
-//    private NavigationBarView.OnItemSelectedListener mSelectedListener = new NavigationBarView.OnItemSelectedListener() {
-//        @Override
-//        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//
-//            switch(item.getItemId()){
-//
-//                case R.id.homepage:
-//
-//                    FragmentManager fm = getSupportFragmentManager();
-//                    Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-//                    if(fragment == null){
-//                        //create a fragment
-//                        fragment = createFragment();
-//                        fm.beginTransaction()
-//                                .add(R.id.fragment_container, fragment)
-//                                .commit();
-//                    }
-////                    ProductListFragment fragment = new ProductListFragment();
-////
-////                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-////                    fragmentTransaction.replace(R.id.fragment_container, fragment);
-////                    fragmentTransaction.commit();
-//
-//                    return true;
-//
-//
-//
-//            }
-//
-//
-//            return false;
-//        }
-//    };
-//
-//    @Override
-//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-//
-//        FragmentManager fm = getSupportFragmentManager();
-//        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-//
-//        switch(item.getItemId()){
-//
-//            case R.id.homepage:
-//                //FragmentManager fm = getSupportFragmentManager();
-//                //Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-//                if(fragment == null){
-//                    //create a fragment
-//                    fragment = createFragment();
-//                    fm.beginTransaction()
-//                            .add(R.id.fragment_container, productListFragment)
-//                            .commit();
-//                }
-//                return true;
-//
-//            case R.id.menu:
-////                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, productListFragment);
-//                return true;
-//
-//            case R.id.person:
-////                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, productListFragment);
-//                return true;
-//
-//            default:
-//                return super.onOptionsItemSelected(item);
-//
-//        }
-//
-//    }
 }
